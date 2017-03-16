a669e7d4f8786885386eb38970332e77365f34a8
Tejveer <tejveer.pro@gmail.com>
2017-03-16 13:14:44 -0700

    boilerplate

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+// Create our app
+var app = express();
+
+app.use(express.static('public'));
+
+app.listen(8000, function () {
+  console.log('Express server is up on port 8000');
+});
